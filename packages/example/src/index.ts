9dc101b96c45b2b02521105572cb81679c66f3c6
Allen7D <462870781@qq.com>
2026-01-29 19:35:42 +0800

    refactor: 封装重复绘制 paper 示例的方法 drawPaperExample

--- packages/example/src/index.ts
+++ packages/example/src/index.ts
@@ -13,21 +13,23 @@ const createCanvas = (width: number, height: number, id: string) => {
   return canvas;
 }
 
-// 等待 DOM 加载完成
-document.addEventListener('DOMContentLoaded', () => {
-  // 添加画布到页面
-  const canvas = createCanvas(800, 600, 'paper-canvas');
+const drawPaperExample = (width: number, height: number, id: string): paper.PaperScope => {
+  const canvas = createCanvas(width, height, id);
   document.body.appendChild(canvas);
   const paperScope = new paper.PaperScope();
   paperScope.setup(canvas);  // 初始化 Paper.js
   createShapes(paperScope);
 
+  return paperScope;
+}
+
+// 等待 DOM 加载完成
+document.addEventListener('DOMContentLoaded', () => {
+  // 添加画布到页面
+  const paperScope = drawPaperExample(800, 600, 'paper-canvas');
+
   // 添加画布2到页面
-  const canvas2 = createCanvas(800, 600, 'paper-canvas2');
-  document.body.appendChild(canvas2);
-  const paperScope2 = new paper.PaperScope();
-  paperScope2.setup(canvas2);  // 初始化 Paper.js
-  createShapes(paperScope2);
+  const paperScope2 = drawPaperExample(800, 600, 'paper-canvas2');
 
   // TODO: 在界面上对 element 进行快速高亮
   globalThis.__PAPER_SCOPE__ = {
